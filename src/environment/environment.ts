18a8f1288159ffa7fdf499024df42be30c5fd983
Aristocrat4 <galadanelia1@gmail.com>
2023-04-30 23:39:30 +0400

    create ng project and add some folders in app module

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  baseUrl: '',
+  firebaseConfig: {
+    // apiKey: 'AIzaSyBe9xUO8JQET_a0Fb6n14MWxg4NTeNO1kM',
+    // authDomain: 'medbooker-gala.firebaseapp.com',
+    // projectId: 'medbooker-gala',
+    // storageBucket: 'medbooker-gala.appspot.com',
+    // messagingSenderId: '751856609212',
+    // appId: '1:751856609212:web:3ada396febaf61a0d0edb9',
+  },
+};
